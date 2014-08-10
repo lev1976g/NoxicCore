420aff38f67c6e84eefd9b6ca7572a20917c046d
zdroid9770 <zdroid9770@yahoo.com>
2014-08-10 11:29:51 -0500

    Added missing worldstring
    Corrected .sql name

--- sql/world_updates/2014-08-05_01_worldstring.sql
+++ sql/world_updates/2014-08-05_01_worldstring.sql
@@ -1,6 +1,7 @@
-DELETE FROM `worldstring_tables` WHERE `entry` between 85 and 88;
+DELETE FROM `worldstring_tables` WHERE `entry` between 85 and 89;
 INSERT INTO `worldstring_tables` (`text`) VALUES 
 ('Trick or Treat!'),
+('What can I do at an inn?'),
 ('You must be at least level %u to buy Arena charter');
 
 UPDATE `world_db_version` SET `LastUpdate` = '2014-08-05_01_worldstring' WHERE `LastUpdate` = '2014-08-05_00_worldstring';
